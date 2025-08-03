cf4b7ac4b62572db82473bb5906534eeb0c1b378
susmitha-128 <120056912+susmitha-128@users.noreply.github.com>
2025-08-03 22:18:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+const snackPrices = {
+  "Idli": 30,
+  "Chapathi": 30,
+  "Bonda": 35,
+  "Dosa": 50,
+  "upma": 25
+};
+
+let total = 0;
+
+function addSnack() {
+  const snack = document.getElementById("snack").value;
+  const qty = parseInt(document.getElementById("qty").value);
+  if (qty <= 0) return;
+
+  const price = snackPrices[snack] * qty;
+  total += price;
+
+  const billBody = document.getElementById("bill-body");
+  const row = document.createElement("tr");
+  row.innerHTML = `<td>${snack}</td><td>${qty}</td><td>₹${price.toFixed(2)}</td>`;
+  billBody.appendChild(row);
+
+  document.getElementById("total").textContent = `Total: ₹${total.toFixed(2)}`;
+}
+
+function checkout() {
+   const container = document.querySelector(".container");
+   const cont = document.getElementById("cont");
+   const cont_two = document.querySelector(".cont_two");
+   container.style.display ="none";
+   cont_two.style.display = "block";
+cont.textContent = `Thank you! Your total is ₹${total.toFixed(2)}.\n visit us again.`;
+}
